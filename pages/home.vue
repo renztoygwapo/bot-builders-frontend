3bd4f7c14ee33e8330e3120984b5423eaa7e9fd7
Florence Comajes <renztoygwapo@gmail.com>
2022-08-19 20:36:59 +0800

    update text

--- pages/home.vue
+++ pages/home.vue
@@ -8,7 +8,7 @@ definePageMeta({
 <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div class="max-w-md w-full space-y-8">
     <div class="text-center">
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome Home</h2>
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Hello Philippines!</h2>
       <nuxt-link class="mt-2 text-blue-600" to="crud">-> Go to CRUD</nuxt-link>
     </div>
   </div>
